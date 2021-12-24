af964c5c6e3f1f4a10d8437154f7cded41edae6e
aeshcherbakova <aeshcherbakova@yandex.ru>
2021-12-24 23:32:15 +0300

    1.6 - вывести кол-во файлов и скрытых файлов в home

--- lab4/scripts/num_of_files_home.sh
+++ lab4/scripts/num_of_files_home.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo Домашний каталог пользователя
-echo $USER
-echo содержит обычных файлов:
+echo -e "Домашний каталог пользователя\n$USER\nсодержит обычных файлов:"
 find / -maxdepth 1 -type f -not -name ".*" | wc -l
 echo скрытых файлов:
 find / -maxdepth 1 -type f -name ".*" | wc -l
