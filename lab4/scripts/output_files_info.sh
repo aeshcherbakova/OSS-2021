6d6a0a0f31e77f0d1d6437626f44255d2a50d1ab
aeshcherbakova <aeshcherbakova@yandex.ru>
2021-12-25 00:37:55 +0300

    1.11 - Вывести инфо о файлах, каталогах, ссылках

---
+++ lab4/scripts/output_files_info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo Каталоги:
+ls -la | grep ^d
+echo Обычные файлы:
+ls -la | grep ^-
+echo Символьные ссылки:
+ls -la | grep ^l
+echo Символьные устройства:
+ls -la | grep ^c
+echo Блочные устройства:
+ls -ls | grep ^b
